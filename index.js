5c2349b50dd2e017ce3d2cfe56ebbed6e1e410b1
Vladimir V <vavan.v@gmail.com>
2021-04-18 18:28:16 -0400

    update

--- index.js
+++ index.js
@@ -5,9 +5,30 @@ const http = require("http");
 const https = require("https");
 const url = require("url");
 const StringDecoder = require("string_decoder").StringDecoder;
+var fs = require("fs");
 
 var config = require("./config");
-var fs = require("fs");
+var _data = require("./lib/data");
+
+// TESTING
+// @TODO delete this
+
+// _data.create("test", "newFile", { foo: "bar" }, function (err) {
+//   console.log("This is an error", err);
+// });
+
+// _data.read("test", "newFile", function (err, data) {
+//   console.log("This is an error", err);
+//   console.log("This is a data", data);
+// });
+
+// _data.update("test", "newFile", { fizz: "buzz" }, function (err) {
+//   console.log("This is an error", err);
+// });
+
+// _data.delete("test", "newFile", function (err) {
+//   console.log(`This was the error ${err}`);
+// });
 
 // Instantiate the http server
 var httpServer = http.createServer(function (req, res) {
